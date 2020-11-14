93fe72c8bddb47810c10250a262d25d803b71d75
Arsh Anwar <45912425+d4rk-lucif3r@users.noreply.github.com>
2020-11-15 02:04:42 +0530

    update clone link

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Sending..................................Message"
-git clone git@github.com:fabianonline/telegram.sh.git
+git clone https://github.com/fabianonline/telegram.sh.git
 cd telegram.sh
 ./telegram -t $TELEGRAM_TOKEN -c $TELEGRAM_CHAT -f telegram "Sentttttttttttttttt Frommmmmmmmmmmmmm Circle Ci................."
 echo "Done............................"
